b2fc65fa581ddd066a25a358fda915ea6c64ce59
@sogek1ng__ <andrereynaldi99@gmail.com>
2025-08-24 01:59:39 +0700

    Update make-image.sh

--- make-image.sh
+++ make-image.sh
@@ -71,8 +71,8 @@ configure_profile_packages() {
     fi
 
     if [[ "${TYPE:-}" == "OPHUB" ]]; then
-        PACKAGES+=" luci-app-amlogic btrfs-progs kmod-fs-btrfs"
-        EXCLUDED+=" -kmod-nls-cp437 -kmod-nls-iso8859-1 -procd-ujail -kmod-tun -kmod-amazon-ena -kmod-e1000e -kmod-vmxnet3 -kmod-rtc-rx8025 -kmod-i2c-mux-pca954x -kmod-gpio-pca953x -partx-utils -kmod-wdt-sp805 -kmod-mvneta -kmod-mvpp2 -kmod-fsl-dpaa1-net -kmod-fsl-dpaa2-net -kmod-fsl-enetc-net -kmod-dwmac-imx -kmod-fsl-fec -kmod-dwmac-rockchip -kmod-dwmac-sun8i -kmod-phy-aquantia -kmod-phy-broadcom -kmod-phy-marvell -kmod-phy-marvell-10g -kmod-sfp -kmod-atlantic -kmod-bcmgenet -kmod-octeontx2-net -kmod-renesas-net-avb -kmod-phy-realtek -kmod-phy-smsc"
+        PACKAGES+=" busybox-selinux libselinux libsepol muslfts luci-app-amlogic btrfs-progs kmod-fs-btrfs"
+        EXCLUDED+=" -libiwinfo-data -libiwinfo -rpcd-mod-iwinfo -kmod-nls-cp437 -kmod-nls-iso8859-1 -procd-ujail -kmod-tun -kmod-amazon-ena -kmod-e1000e -kmod-vmxnet3 -kmod-rtc-rx8025 -kmod-i2c-mux-pca954x -kmod-gpio-pca953x -partx-utils -kmod-wdt-sp805 -kmod-mvneta -kmod-mvpp2 -kmod-fsl-dpaa1-net -kmod-fsl-dpaa2-net -kmod-fsl-enetc-net -kmod-dwmac-imx -kmod-fsl-fec -kmod-dwmac-rockchip -kmod-dwmac-sun8i -kmod-phy-aquantia -kmod-phy-broadcom -kmod-phy-marvell -kmod-phy-marvell-10g -kmod-sfp -kmod-atlantic -kmod-bcmgenet -kmod-octeontx2-net -kmod-renesas-net-avb -kmod-phy-realtek -kmod-phy-smsc"
     elif [[ "${TYPE:-}" == "ULO" ]]; then
         PACKAGES+=" luci-app-amlogic"
         EXCLUDED+=" -procd-ujail"
